129543947c6fa3f3b886a8d5c4022ede79421107
atoudghi <ayoub.toudghi@auditeur.ec-lyon.fr>
2025-09-18 13:19:19 +0200

    Initial commit - Flask viz app

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # Données d'exemple
+    data = [5, 7, 2, 4, 9, 10]
+    
+    # Créer le graphique
+    sns.set(style="whitegrid")
+    plt.figure(figsize=(6,4))
+    plt.plot(data, marker='o')
+    plt.title("Exemple de graphique")
+    
+    # Sauvegarder l'image dans le dossier static
+    graph_path = os.path.join('static', 'graph.png')
+    plt.savefig(graph_path)
+    plt.close()
+    
+    return render_template('index.html', graph_url=graph_path)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
